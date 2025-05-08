1a645fc293e3856c9024005a143aa2cd10d3461b
sristi15-22 <209684561+sristi15-22@users.noreply.github.com>
2025-05-08 14:41:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+  alert("Keep reading and growing, Sristi!");
+}
+
+function showMore() {
+  const info = document.getElementById("more-info");
+  info.classList.remove("hidden");
+}
